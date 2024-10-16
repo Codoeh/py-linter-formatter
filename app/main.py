1feb1be45262d6357a737d3950b13c0b804f0591
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2024-10-16 21:59:55 +0200

    Flake8 rules add

--- app/main.py
+++ app/main.py
@@ -7,14 +7,17 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8"
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors":
-        [format_linter_error(error) for error in errors],
-    "path": file_path,
-    "status": "passed"
-    if errors == []
-    else "failed"
+    return {
+        "errors":
+            [format_linter_error(error) for error in errors],
+            "path": file_path,
+            "status": "passed"
+            if errors == []
+            else "failed"
     }
+
 
 def format_linter_report(linter_report: dict) -> list:
     return [
