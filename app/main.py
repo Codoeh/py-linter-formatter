f69d3c41b4e53a1ca4fd204154265bfb882d50c9
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2024-10-16 15:46:50 +0200

    solution2

--- app/main.py
+++ app/main.py
@@ -10,12 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors":
-        [{
-        "line": error.get("line_number"),
-        "column": error.get("column_number"),
-        "message": error.get("text"),
-        "name": error.get("code"),
-        "source": "flake8"} for error in errors],
+        [{format_linter_error(error)} for error in errors],
     "path": file_path,
     "status": "passed"
     if errors == []
@@ -23,22 +18,16 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     }
     pass
 
-
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {"errors": [], "path": list(linter_report)[0], "status": "passed"},
-        {
-            "errors": [
-                {
-                    "line": linter_report.get("line_number"),
-                    "column": linter_report.get("column_number"),
-                    "message": linter_report.get("text"),
-                    "name": linter_report.get("code"),
-                    "source": "flake8"}
+            {"errors": [], "path": list(linter_report)[0], "status": "passed"},
+            {
+                "errors": [
+                    {format_single_linter_file(linter_report["errors"])}
                     for errors in linter_report],
-            "path": list(linter_report)[1],
-            "status": "passed"
-            if "errors" == []
-            else "failed"},
-    ]
+                "path": list(linter_report)[1],
+                "status": "passed"
+                if "errors" == []
+                else "failed"},
+        ]
     pass
